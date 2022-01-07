911aea7ce193e7b44816fb10cbcbf64c83a35177
Martin Olivier <martin.olivier@live.fr>
2022-01-07 14:12:59 +0100

    fix: CI

--- test/myDynLib.cpp
+++ test/myDynLib.cpp
@@ -4,14 +4,12 @@
 export_symbol double pi_value = 3.14159;
 export_symbol void *ptr = (void *)1;
 
-export_symbol {
-    double adder(double a, double b)
-    {
-        return a + b;
-    }
+export_symbol double adder(double a, double b)
+{
+    return a + b;
+}
 
-    void printHello()
-    {
-        std::cout << "Hello!" << std::endl;
-    }
+export_symbol void printHello()
+{
+    std::cout << "Hello!" << std::endl;
 }
