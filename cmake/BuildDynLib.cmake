c9725c54327b138bcee84862566ba1fc1e08e3cb
Martin Olivier <martin.olivier@live.fr>
2022-01-07 21:39:14 +0100

    fix: CI

--- cmake/BuildDynLib.cmake
+++
@@ -1,7 +0,0 @@
-cmake_minimum_required(VERSION 3.14)
-
-add_library(dynlib SHARED test/myDynLib.cpp)
-
-target_include_directories(dynlib PUBLIC .)
-
-set_target_properties(dynlib PROPERTIES PREFIX "")
