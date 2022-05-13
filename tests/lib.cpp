e09477bdd412e1c0b77e8677d08f524033db45cf
Martin Olivier <58467105+martin-olivier@users.noreply.github.com>
2022-05-13 09:59:52 +0200

    fix: gcc warnings when initializing extern variables

--- tests/lib.cpp
+++ tests/lib.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include "dylib.hpp"
 
-DYLIB_API double pi_value = 3.14159;
-DYLIB_API void *ptr = (void *)1;
+DYLIB_API inline double pi_value = 3.14159;
+DYLIB_API inline void *ptr = (void *)1;
 
 DYLIB_API double adder(double a, double b) {
     return a + b;
